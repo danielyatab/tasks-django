763a19f41338158727e7afb4cfab918dee23333b
danielyatab <yataco-126@hotmail.com>
2024-03-14 16:10:56 -0500

    changed yml File

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const apiTasks = axios.create({ baseURL: "http://localhost:8000/tasks/api/v1/tasks" })
+const apiTasks = axios.create({ baseURL: "http://localhost:8000/tasks", timeout: 5000 })
 
 export const getAllTasks = () => apiTasks.get("/")
 export const getTask = (id) => apiTasks.get(`/${id}/`)
