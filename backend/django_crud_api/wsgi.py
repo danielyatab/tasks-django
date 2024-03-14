3b7ff85f32c47da671afa95b24643589da9ae579
danielyatab <yataco-126@hotmail.com>
2024-03-14 16:29:48 -0500

    changed wsgi file

--- backend/django_crud_api/wsgi.py
+++ backend/django_crud_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 settings_module = 'django_crud_api.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'django_crud_api.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_crud_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
