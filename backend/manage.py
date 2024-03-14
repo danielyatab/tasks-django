62c2eb39d9860364f39af3df5befefc8aa0615f9
danielyatab <yataco-126@hotmail.com>
2024-03-14 16:31:42 -0500

    second changed wsgi file

--- backend/manage.py
+++ backend/manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     settings_module = 'django_crud_api.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'django_crud_api.settings'
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_crud_api.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
